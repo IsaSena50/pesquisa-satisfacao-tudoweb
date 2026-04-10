f7c7c4258a3db2a4ae832a1bcb6977b22b16d6bf
Isoliana Sena <isaferreirasena@gmail.com>
2026-04-10 17:24:35 -0300

    Update pesquisa.py

--- pesquisa.py
+++ pesquisa.py
@@ -6,20 +6,20 @@ count_excelente = 0
 count_ruim = 0
 
 # Definimos o número de entrevistados.
-# Para o teste, usamos 10. Para o projeto final, mude para 50.
-num_entrevistados = 10 
 
-print("--- Início da Pesquisa TudoWeb ---")
+num_entrevistados = 50
+
+print("--- Sistema de Pesquisa TudoWeb ---")
 print(f"Total de entrevistados a serem ouvidos: {num_entrevistados}")
 
 # Estrutura de Repetição FOR
-# 'range(1, num_entrevistados + 1)' garante que o contador vá de 1 até o número total
+
 for i in range(1, num_entrevistados + 1):
     print(f"\nEntrevistado nº {i}")
     
     # Coleta de dados
     nome = input("Digite o nome: ")
-    idade = input("Digite a idade: ") # Recebemos como texto para não travar se digitarem letra
+    idade = input("Digite a idade: ") # Entradade idade
 
     # Coleta da opinião com o menu
     print("Por favor, avalie nosso atendimento:")
@@ -34,8 +34,7 @@ for i in range(1, num_entrevistados + 1):
         count_excelente += 1 # Soma 1 ao contador de excelente
     elif opiniao == "3":
         count_ruim += 1 # Soma 1 ao contador de ruim
-    # A opção "2" não precisa ser contada de acordo com o pedido.
-    # Qualquer outra resposta diferente de 1 ou 3 não altera os contadores.
+  
 
 # Fim da coleta, exibição dos resultados
 print("\n" + "="*40)
@@ -44,4 +43,4 @@ print("="*40)
 print(f"a) Quantidade de respostas 'EXCELENTE': {count_excelente}")
 print(f"b) Quantidade de respostas 'RUIM':      {count_ruim}")
 print("="*40)
-print("Obrigado pela participação!")
+print("Obrigado pela participação pesquisa finalizada com sucesso!")
